531ff64cba21d4e32dd92239cf29dfe70c606e0a
Esko Oramaa <ezzkoram@gmail.com>
2017-10-06 16:45:56 +0300

    Add FibonacciSliceRecursive function

--- fibonacci.go
+++ fibonacci.go
@@ -11,3 +11,16 @@ func FibonacciSlice(n uint64) []uint64 {
 	}
 	return result
 }
+
+func fibonacciSliceReucrsiveHelper(left, a, b uint64) []uint64 {
+	if left == 0 {
+		return []uint64{}
+	}
+	return append([]uint64{b}, fibonacciSliceReucrsiveHelper(left-1, b, a+b)...)
+}
+
+// FibonacciSliceRecursive returns n first numbers from fibonacci series
+// This variant will use recursion instead of a loop to construct the series
+func FibonacciSliceRecursive(n uint64) []uint64 {
+	return fibonacciSliceReucrsiveHelper(n, 0, 1)
+}
--- fibonacci_test.go
+++ fibonacci_test.go
@@ -26,6 +26,27 @@ func TestFibonacciSlice(t *testing.T) {
 	}
 }
 
+func TestFibonacciSliceRecursive(t *testing.T) {
+	tests := []struct {
+		n        uint64
+		expected []uint64
+	}{
+		{0, []uint64{}},
+		{1, []uint64{1}},
+		{2, []uint64{1, 1}},
+		{4, []uint64{1, 1, 2, 3}},
+		{7, []uint64{1, 1, 2, 3, 5, 8, 13}},
+		{9, []uint64{1, 1, 2, 3, 5, 8, 13, 21, 34}},
+	}
+
+	for _, tt := range tests {
+		actual := FibonacciSliceRecursive(tt.n)
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("fibonacciSlice(%d): expected %v, actual %v", tt.n, tt.expected, actual)
+		}
+	}
+}
+
 func benchmarkFibonacciSliceN(n uint64, b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FibonacciSlice(n)
@@ -34,3 +55,12 @@ func benchmarkFibonacciSliceN(n uint64, b *testing.B) {
 func BenchmarkFibonacciSlice1(b *testing.B)  { benchmarkFibonacciSliceN(1, b) }
 func BenchmarkFibonacciSlice5(b *testing.B)  { benchmarkFibonacciSliceN(5, b) }
 func BenchmarkFibonacciSlice15(b *testing.B) { benchmarkFibonacciSliceN(15, b) }
+
+func benchmarkFibonacciSliceRecursiveN(n uint64, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		FibonacciSliceRecursive(n)
+	}
+}
+func BenchmarkFibonacciSliceRecursive1(b *testing.B)  { benchmarkFibonacciSliceRecursiveN(1, b) }
+func BenchmarkFibonacciSliceRecursive5(b *testing.B)  { benchmarkFibonacciSliceRecursiveN(5, b) }
+func BenchmarkFibonacciSliceRecursive15(b *testing.B) { benchmarkFibonacciSliceRecursiveN(15, b) }
